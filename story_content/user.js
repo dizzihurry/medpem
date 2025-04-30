08f12395a7749d6e9cbfb45f4150e17304af1717
vadila <vadilazikrarahmaa@gmail.com>
2025-04-30 20:37:00 +0700

    first commit

---
+++ story_content/user.js
@@ -0,0 +1,18 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5nCJOOP0U2h":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById( “bgsong”);
+audio. src="musik.mp3" ;
+audio.load;
+audio.play() ;
+}
+
